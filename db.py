4e9da44bc1cb1a14bbaceee41717016a76708d6f
khaled youssif <Khaled El-Mansy>
2026-07-24 07:26:59 +0300

    fix DB_SERVER variable definition

--- db.py
+++ db.py
@@ -3,16 +3,19 @@ db.py
 المسؤول الوحيد عن فتح اتصال مع قاعدة بيانات SQL Server.
 كل ملف تاني في المشروع هيستدعي get_connection() من هنا لما يحتاج يكلم الداتابيز.
 """
-
 import os
 import pymssql
+
+# قراءة البيانات من متغيّرات بيئة Render
+DB_SERVER = os.getenv('DB_SERVER')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
 
 def get_connection():
     return pymssql.connect(
         server=DB_SERVER,
-        database=DB_NAME,
         user=DB_USER,
         password=DB_PASSWORD,
-        port=1433,
-        as_dict=True
-)
+        database=DB_NAME
+    )
